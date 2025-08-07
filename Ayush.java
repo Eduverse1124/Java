463c91157653c68a64708cc104257baac7ee02e3
Eduverse1124 <jagranproject1124@gmail.com>
2025-08-07 21:52:56 +0530

    Test Push

---
+++ Ayush.java
@@ -0,0 +1,19 @@
+// Java Object Oriented Programming Code 
+
+public class Ayush {
+    
+    // instance variable
+    public int a = 1124;
+
+    // instance method 
+    public void ayu() {
+        System.out.println("Magic Number is : " + a);
+    }
+
+    public static void main(String[] args) {
+        Ayush obj = new Ayush();
+        System.out.println("=== Main Start ===");
+        obj.ayu();
+    }
+}
+
